84396d4a8defe642e78c7eb2fadd5e6de35321d8
Andrei Vsyakikh <hero@Andreis-MacBook-Air.local>
2021-05-27 13:11:10 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import itunespy
+#kind, collectionName, trackName, collectionPrice, trackPrice, primaryGenreName, trackCount, trackNumber, releaseDate.
+artist = itunespy.search_artist('The Beatles')  # Returns a list
+albums = artist[0].get_albums()  # Get albums from the first result
+
+for album in albums:
+    print("==============albumname==============")
+    print(album.collection_name, album.primaryGenreName, album.track_count, album.release_date, album.collection_id)
+    print("vvvvv----------------album------------vvvvv")
+    album = itunespy.search_album(album.collection_id)  # Returns a list
+    tracks = album[0].get_tracks()  # Get tracks from the first result
+
+    for track in tracks:
+        print( track.artist_name + ': '  + track.track_name + str(track.get_track_time_minutes()) + " price " + str(track.track_price) + str(track.kind) + str())
+    print('Total playing time: ' + str(album[0].get_album_time()))
