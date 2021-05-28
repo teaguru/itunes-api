cd3c7c8ec93f8c3b31205e17f6b2a92c6b62823a
Vsyakikh Andrey <teaguru@bk.ru>
2021-05-28 10:25:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ albums = artist[0].get_albums()  # Get albums from the first result
 for album in albums:
     print("==============albumname==============")
     print(album.collection_name, album.primaryGenreName, album.track_count, album.release_date, album.collection_id)
+    print(album)
     print("vvvvv----------------album------------vvvvv")
-    album = itunespy.search_album(album.collection_id)  # Returns a list
+
+
+    album = (itunespy.search_album(album.collection_name))
+    print(album)# Returns a list
     tracks = album[0].get_tracks()  # Get tracks from the first result
 
     for track in tracks:
-        print( track.artist_name + ': '  + track.track_name + str(track.get_track_time_minutes()) + " price " + str(track.track_price) + str(track.kind) + str())
-    print('Total playing time: ' + str(album[0].get_album_time()))
+      print( track.artist_name + ': '+ track.track_name + str(track.get_track_time_minutes()) + " price " + str(track.track_price) + str(track.kind) + str())
+      print('Total playing time: ' + str(album[0].get_album_time()))
